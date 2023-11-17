5163cfd0827c39b4b080fcee25d299171a273a75
mandog66 <53986210+mandog66@users.noreply.github.com>
2023-11-17 22:06:16 +0800

    刪除圖片更動

--- check_img.py
+++ check_img.py
@@ -2,14 +2,18 @@ import cv2
 import os
 import numpy as np
 import keras.utils as image
-from PIL import Image
 
 root_dir = os.getcwd()
+try:
+    os.makedirs("del_images")
+    print("del_images folder be created")
+except OSError:
+    print("del_images folder already exist")
 
 
 def img_process(picture):
     # 圖片前置處理
-    img = cv2.imread(root_dir + "\\" + picture, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(picture, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (24, 24))
     img = image.img_to_array(img)
     img = img.reshape(24, 24)
@@ -18,10 +22,11 @@ def img_process(picture):
 
 class P():
     # 圖片資訊
-    def __init__(self, check, img_array, img_name):
+    def __init__(self, check, img_array, img_name, img_format):
         self.check = check
         self.img_array = img_array
         self.img_name = img_name
+        self.img_format = img_format
 
 
 img_temp = []   # 圖片物件陣列
@@ -32,18 +37,19 @@ img_name = input("輸入圖片名稱 : ")
 
 # 圖片前處理後放到圖片物件陣列
 for pictures in os.listdir(root_dir):
-    if pictures.split(".")[1] in file_extension:
-        print(f"\r前處理第 {count} 張圖片中", end='', flush=True)
-        img_array = img_process(pictures)
-        img_temp.append(P(False, img_array, pictures))
-        count += 1
+    if os.path.isfile(os.path.join(root_dir, pictures)):
+        if pictures.split(".")[1] in file_extension:
+            print(f"\r前處理第 {count} 張圖片中", end='', flush=True)
+            img_array = img_process(pictures)
+            img_format = pictures.split(".")[1]
+            img_temp.append(P(False, img_array, pictures, img_format))
+            count += 1
 
 print("\n圖片總數 : ", len(img_temp))
 
 index = 1   # 切割圖片物件陣列
 name = 1    # 圖片檔名
 del_count = 0   # 重複圖片的數量
-del_array = []
 
 # 重複的圖片要不要顯示
 show_del_img = input("是否顯示被刪除的圖片(y/n) : ")
@@ -67,14 +73,9 @@ for img in img_temp:
             if result == True:
                 check_img.check = True
                 if show_del_img == "y":
-                    try:
-                        open_img = Image.open(check_img.img_name)
-                        open_img.show()
-                    except:
-                        open_img = cv2.imread(check_img.img_name)
-                        cv2.imshow("remove_img", open_img)
-                        cv2.waitKey(0)
-                        cv2.destroyAllWindows()
+                    del_img = cv2.imread(check_img.img_name)
+                    cv2.imwrite("./del_images/del_img_{}.{}".format(
+                        del_count + 1, check_img.img_format))
                 elif show_del_img == "n":
                     pass
                 os.remove(check_img.img_name)
--- img_process.py
+++ img_process.py
@@ -8,7 +8,7 @@ fileExtension = ["jpg", "JPG", "jpeg", "jfif", "png", "PNG"]
 
 def img_process(picture):
     # 圖片前置處理
-    img = cv2.imread(root_dir + "\\" + picture, cv2.IMREAD_GRAYSCALE)
+    img = cv2.imread(picture, cv2.IMREAD_GRAYSCALE)
     img = cv2.resize(img, (24, 24))
     img = image.img_to_array(img)
     img = img.reshape(24, 24)
@@ -17,12 +17,12 @@ def img_process(picture):
 
 def file_extension():
     for pictures in os.listdir(root_dir):
-        print(pictures, end="\t")
-        # print(pictures.split("."))
-        if pictures.split(".")[1] in fileExtension:
-            print("True")
-        else:
-            print("False")
+        if os.path.isfile(os.path.join(root_dir, pictures)):
+            print(pictures, end="\t")
+            if pictures.split(".")[1] in fileExtension:
+                print("True")
+            else:
+                print("False")
 
 
 def open_img():
@@ -45,7 +45,23 @@ def open_img():
     cv2.destroyAllWindows()
 
 
+def del_dir():
+    try:
+        os.makedirs("del_images")
+        print("del_images folder be created")
+    except OSError:
+        print("del_images folder exist")
+
+
+def copy_img(i):
+    img = cv2.imread("check_img_763.png")
+    cv2.imwrite("./del_images/del_image_{}.png".format(i), img)
+
+
 if __name__ == "__main__":
     # img = img_process("check_img_763.png")
     # file_extension()
-    open_img()
+    # open_img()
+    # del_dir()
+    copy_img(1)
+    copy_img(2)
