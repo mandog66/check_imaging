a5c022f72977e888f17de60873ef9e6a630ce686
mandog66 <53986210+mandog66@users.noreply.github.com>
2023-11-18 14:48:46 +0800

    變更圖片名字方式

--- check_img.py
+++ check_img.py
@@ -4,6 +4,9 @@ import numpy as np
 import keras.utils as image
 
 root_dir = os.getcwd()
+img_temp = []   # 圖片物件陣列
+count = 1   # 圖片張數
+file_extension = ["jpg", "JPG", "jpeg", "jfif", "png", "PNG"]   # 副檔名
 try:
     os.makedirs("del_images")
     print("del_images folder be created")
@@ -29,10 +32,6 @@ class P():
         self.img_format = img_format
 
 
-img_temp = []   # 圖片物件陣列
-count = 1   # 圖片張數
-file_extension = ["jpg", "JPG", "jpeg", "jfif", "png", "PNG"]   # 副檔名
-
 img_name = input("輸入圖片名稱 : ")
 
 # 圖片前處理後放到圖片物件陣列
@@ -51,17 +50,6 @@ index = 1   # 切割圖片物件陣列
 name = 1    # 圖片檔名
 del_count = 0   # 重複圖片的數量
 
-# 重複的圖片要不要顯示
-# show_del_img = input("是否顯示被刪除的圖片(y/n) : ")
-# if show_del_img != "y" and show_del_img != "n":
-#     while (1):
-#         show_del_img = input("是否顯示被刪除的圖片(y/n) : ")
-#         if show_del_img == "y" or show_del_img == "n":
-#             break
-#         else:
-#             print("再輸入一次(y/n) : ")
-#             continue
-
 # 檢查有沒有重複的圖片
 # img 是要檢查的圖片
 # check_img 是要比對的圖片
@@ -76,24 +64,13 @@ for img in img_temp:
                 cv2.imwrite("./del_images/del_img_{}.{}".format(
                     del_count + 1, check_img.img_format))
 
-                # 刪除圖片備份到 del_images
-                # if show_del_img == "y":
-                #     del_img = cv2.imread(check_img.img_name)
-                #     cv2.imwrite("./del_images/del_img_{}.{}".format(
-                #         del_count + 1, check_img.img_format))
-                # elif show_del_img == "n":
-                #     pass
-
                 # 刪除圖片
                 os.remove(check_img.img_name)
                 del_count += 1
 
     # 更改圖片名字
     if img.check == False:
-        if img.img_name.endswith(".png") or img.img_name.endswith(".PNG"):
-            os.rename(img.img_name, img_name + str(name) + ".png")
-        else:
-            os.rename(img.img_name, img_name + str(name) + ".jpg")
+        os.rename(img.img_name, "LO_{}.{}".format(name, img.img_format))
         print(f"\r檢查第 {name} 張", end='', flush=True)
         name += 1
     index += 1
